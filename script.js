c3d59f5283c526eacc93945b441343d69d9d1c9b
Keshav Madhav <keshav2552003@gmail.com>
2024-02-13 20:03:28 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,108 @@
+const end = Date.now() + 15 * 1000;
+
+// go Buckeyes!
+const colors = ["#bb0000", "#ffffff"];
+
+const run = () => {
+  const defaults = {
+    spread: 360,
+    ticks: 100,
+    gravity: 0,
+    decay: 0.94,
+    startVelocity: 30,
+    shapes: ["heart"],
+    colors: ["FFC0CB", "FF69B4", "FF1493", "C71585"]
+  };
+
+  window.confetti({
+    ...defaults,
+    particleCount: 10,
+    scalar: 1
+  });
+
+  window.confetti({
+    ...defaults,
+    particleCount: 50,
+    scalar: 2
+  });
+
+  window.confetti({
+    ...defaults,
+    particleCount: 25,
+    scalar: 3
+  });
+
+  window.confetti({
+    ...defaults,
+    particleCount: 30,
+    scalar: 4
+  });
+
+  
+};
+
+const yesButton = document.getElementById('yes');
+const noButton = document.getElementById('no');
+
+const text = document.getElementById('text');
+const gif = document.getElementById('gif');
+const usImg = document.getElementById('us');
+
+var index = 0;
+
+const noPhrases = [
+  "No",
+  "Are you sure?",
+  "I am asking nicely though",
+  "Please please?",
+  "I'll give you a kiss",
+  "Okay, I'll buy you momos",
+  "Cmon babyyy",
+  "Pwease yashuu",
+  "I'll die if you dont say yes",
+  "Oh no! I am dead",
+  "I am a ghost now",
+  "I'll haunt you if you dont say yes",
+  "*ghost noises*",
+  "*begging ghost noises*",
+  "Tujhe bridgerton ki kasam",
+  "Last chance",
+  "Iske baad button hi htadega mai",
+];
+
+yesButton.addEventListener('click', () => {
+  text.textContent = 'Yaaaaaaaaayyyyyyyyyy!!!! I love you my valentine <3';
+  gif.src = 'https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif';
+  yesButton.style.display = 'none';
+  noButton.style.display = 'none';
+
+  usImg.style.display = 'block';
+
+  if(Date.now() < end) {
+    run();
+    setTimeout(run, 500);
+  }
+});
+
+noButton.addEventListener('click', () => {
+  index++;
+  if(index === noPhrases.length) {
+    noButton.style.display = 'none';
+  }
+  noButton.textContent = noPhrases[index];
+
+  yesButton.style.scale = 1 + 0.15 * (index +1);
+  noButton.style.scale = 1 - 0.025 * (index +1);
+
+  const buttonWidth = noButton.offsetWidth;
+  const buttonHeight = noButton.offsetHeight;
+
+  const viewportWidth = window.innerWidth || document.documentElement.clientWidth;
+  const viewportHeight = window.innerHeight || document.documentElement.clientHeight;
+
+  const randomTop = Math.random() * (viewportHeight - buttonHeight);
+  const randomLeft = Math.random() * (viewportWidth - buttonWidth);
+
+  noButton.style.top = `${randomTop}px`;
+  noButton.style.left = `${randomLeft}px`;
+});
